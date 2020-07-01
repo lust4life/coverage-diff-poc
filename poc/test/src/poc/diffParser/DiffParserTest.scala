8768466cb4c98603f50ae6df4c71dc48db2cb79e
Jun <lust4life.jun@gmail.com>
2020-07-01 12:30:13 +0800

    test diff parser

--- poc/test/src/poc/diffParser/DiffParserTest.scala
+++ poc/test/src/poc/diffParser/DiffParserTest.scala
@@ -2,9 +2,12 @@ package poc.diffParser
 
 import java.nio.file.{Files, Paths}
 
+import poc.domain.{Changed, Rename}
 import utest._
 
-object DiffParserTest extends TestSuite{
+import scala.collection.Set
+
+object DiffParserTest extends TestSuite {
   val diffParser = new DiffParser
 
   val tests = Tests {
@@ -13,8 +16,29 @@ object DiffParserTest extends TestSuite{
       val in = Files.newInputStream(Paths.get("poc/test/resources/diff.json"))
       val res = diffParser.parse(in)
 
-      assert(res.isEmpty)
-    }
+      val resLen = res.length
+      resLen ==> 4
 
+      val d1 = res(0)
+      assertMatch(d1) {
+        case Changed("src/main/java/com/poc/Bar.java", "java", _) =>
+      }
+
+      val d2 = res(1)
+      assertMatch(d2) {
+        case Changed("src/main/java/com/poc/Foo.java", "java", _) =>
+      }
+
+      val d3 = res(2)
+      assertMatch(d3) {
+        case Changed("src/test/java/FooTest.java", "java", _) =>
+      }
+
+      val changedLines = Set(23.0, 28.0)
+      val d4 = res(3)
+      assertMatch(d4) {
+        case Rename("src/main/java/com/poc/Baz.java", "src/main/java/com/poc/Baz1.java", "java", `changedLines`) =>
+      }
+    }
   }
 }
