2d9d7962dec9aacec6112b1bd24fc1f9579b5554
Jun <lust4life.jun@gmail.com>
2020-07-03 15:38:48 +0800

    ut for file resolver from local file

--- poc/test/src/poc/codeBlock/CodeBlockTest.scala
+++ poc/test/src/poc/codeBlock/CodeBlockTest.scala
@@ -2,33 +2,29 @@ package poc.codeBlock
 
 import utest._
 
-object CodeBlockTest extends TestSuite{
+import scala.util.Try
+
+object CodeBlockTest extends TestSuite {
 
   val tests = Tests {
-    val dummyRange =BlockRange(1,1)
+    val dummyRange = BlockRange(1, 1)
 
     "method's id should concat container's qualification" - {
-      val m = Method(ClassOrInterface("",dummyRange,"a.b.c"),"Foo M1(Bar)",dummyRange)
+      val m = Method(ClassOrInterface("", dummyRange, "a.b.c"), "Foo M1(Bar)", dummyRange)
       val id = m.id
       assert(id == "a.b.c\t#\tFoo M1(Bar)")
     }
 
     "method's file path should be container's" - {
-      val m = Method(ClassOrInterface("a/b/c",dummyRange,""),"",dummyRange)
+      val m = Method(ClassOrInterface("a/b/c", dummyRange, ""), "", dummyRange)
       val filePath = m.filePath
       assert(filePath == "a/b/c")
     }
 
     "block range' begin should <= end" - {
-      try{
-        BlockRange(8,2)
-        assert(false)
-      }catch{
-        case ex => {
-          val msg = ex.getMessage
-          msg ==> "requirement failed: begin should <= end"
-        }
-      }
+      Try {
+        BlockRange(8, 2)
+      }.failed.get.getMessage ==> "requirement failed: begin should <= end"
     }
   }
 }
