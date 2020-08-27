8a527b1550f561a5e018cc05e76d1e33991fa507
Jun <lust4life.jun@gmail.com>
2020-08-27 21:33:16 +0800

    fix test since ci using --depth 1 when clone git

--- poc/test/src/poc/tools/git/GitToolTest.scala
+++ poc/test/src/poc/tools/git/GitToolTest.scala
@@ -29,8 +29,13 @@ object GitToolTest extends TestSuite {
       val tmp = os.temp.dir()
       val gitTool = GitTool(tmp.toIO, pwd.toString())
       gitTool.ensureRepoCloned()
-      gitTool.resetTo("dfcf215804c508e3a3e2abfec191f18aea9eb39e")
-      os.list(tmp, false).length ==> 4
+      val fileCount = os.list(tmp).length
+
+      os.write(tmp / "hello", "world")
+      os.list(tmp).length ==> fileCount + 1
+
+      gitTool.resetTo("master")
+      os.list(tmp).length ==> fileCount
     }
 
     "reset to some version should failed if not cloned" - {
@@ -50,7 +55,8 @@ object GitToolTest extends TestSuite {
       var res = gitTool.diff("master", "master")
       res.out.bytes.length ==> 0
 
-      res = gitTool.diff("master~1", "master")
+      os.write.append(tmp / "README.md", "add some info")
+      res = gitTool.diff("master", ".")
       assert(res.out.bytes.length > 0)
     }
   }
